2c4f0325f18c97281df24ec104589586005e70aa
mariks1 <marat-khasanshin@list.ru>
2025-09-18 17:25:01 +0300

    Initial commit

---
+++ src/main/java/khasanshin/iblab1/IbLab1Application.java
@@ -0,0 +1,13 @@
+package khasanshin.iblab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbLab1Application.class, args);
+    }
+
+}
---
+++ src/test/java/khasanshin/iblab1/IbLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package khasanshin.iblab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
